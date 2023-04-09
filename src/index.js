eb142773b490f01ae530b3ce405ac8948ee16ff0
Brian Skehan <brian.skehan@gmail.com>
2023-04-09 20:55:11 +0100

    table v1

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import FilterablePeakEventTable from './components/FilterablePeakEventTable';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
     <FilterablePeakEventTable/>
   </React.StrictMode>
 );
